8099d8cb195ce383c9b3ef3b787735b348e09ad8
Dzenis <dzenis.hankusic@gmail.com>
2022-04-12 11:01:47 +0200

    Fixed keys

--- config/keys.js
+++ config/keys.js
@@ -1,8 +1,6 @@
-// If you want to use this app you'll need to set up your keys here
-module.exports = {
-  mongoURI:
-    "mongodb://BiggaHD:Test_321@mini-stories-shard-00-00.tnkto.mongodb.net:27017,mini-stories-shard-00-01.tnkto.mongodb.net:27017,mini-stories-shard-00-02.tnkto.mongodb.net:27017/mini-stories?ssl=true&replicaSet=atlas-npi83t-shard-0&authSource=admin&retryWrites=true&w=majority", // mLab URI - [ don't forget to setup a DB user first ]
-  googleClientID:
-    "813156837717-abgvcub8016l4vl6ec71gsk1i377aufr.apps.googleusercontent.com", // Google Client ID
-  googleClientSecret: "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", // GoogleClient Secret
-};
+// Depending of the env choose the file to for your access keys
+if (process.env.NODE_ENV === "production") {
+  module.exports = require("./keys_prod");
+} else {
+  module.exports = require("./keys_dev");
+}
--- config/passport.js
+++ config/passport.js
@@ -1,5 +1,5 @@
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
-const keys = require("./keys");
+const keys = require("./keys-prod");
 const User = require("../models/User");
 
 module.exports = function (passport) {
